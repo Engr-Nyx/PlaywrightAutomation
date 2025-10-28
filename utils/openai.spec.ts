88e901a59c6278d30ac896e2890ed1e88fdf8e2a
Engr-Nyx <arcee.tomaquin@gmail.com>
2025-10-28 21:26:11 +0800

    Update openai.spec.ts

--- utils/openai.spec.ts
+++ utils/openai.spec.ts
@@ -2,7 +2,7 @@ import { request } from "@playwright/test";
 
 export class OpenAi {
   public async compareContentOnText(image: string, prompt: string) {
-    const apiKey = "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";
+    const apiKey = "";
     if (!apiKey) throw new Error("Missing OPEN_AI_API secret");
     const response = await fetch("https://api.openai.com/v1/chat/completions", {
       method: "POST",
