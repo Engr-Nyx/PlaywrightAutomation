a86952d3b8f39e9b9891ab0e50ec62908adb155b
Engr-Nyx <arcee.tomaquin@gmail.com>
2026-02-28 13:09:28 +0800

    Added fixtures

---
+++ fixtures/base.fixtures.ts
@@ -0,0 +1,24 @@
+import { test as base } from '@playwright/test';
+import { LoginPage } from '../pages/auth/login.page';
+import { SignUpPage } from '../pages/auth/signup.page';
+import { DashboardPage } from '../pages/dashboard/dashboard.page';
+
+type fixtures = {
+  loginPage: LoginPage;
+  signUpPage: SignUpPage;
+  dashboardPage: DashboardPage;
+};
+
+export const test = base.extend<fixtures>({
+  loginPage: async ({ page }, use) => {
+    await use(new LoginPage(page));
+  },
+  signUpPage: async ({ page }, use) => {
+    await use(new SignUpPage(page));
+  },
+  dashboardPage: async ({ page }, use) => {
+    await use(new DashboardPage(page));
+  },
+});
+
+export { expect } from '@playwright/test';
